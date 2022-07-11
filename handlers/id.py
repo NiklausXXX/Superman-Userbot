6f7b9aa0c13108755c6d59f820e16e438e2af690
NiklausXXX <108972857+NiklausXXX@users.noreply.github.com>
2022-07-11 12:53:45 +0700

    Update id.py

--- handlers/id.py
+++ handlers/id.py
@@ -8,15 +8,15 @@ from handlers.help import *
 @Client.on_message(filters.me & filters.command("id", ["."]))
 async def id(client: Client, message: Message):
     if message.reply_to_message is None:
-        await message.reply(f"This chat's ID is: {message.chat.id}")
+        await message.reply(f"Chat ID: {message.chat.id}")
     else:
-        test = f"This user's ID is: {message.reply_to_message.from_user.id}\n\nThis chat's ID is: {message.chat.id}"
+        test = f"<b>User ID:</b> {message.reply_to_message.from_user.id}\n\n<b>Chat ID:</b> {message.chat.id}"
         await message.edit_text(test)
 
 
 add_command_help(
     "id",
     [
-        [".id", "To Get User Id."],
+        [".id", "Lihat User ID."],
     ],
 )
